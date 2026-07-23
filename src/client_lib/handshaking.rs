e96342a2eb7142d6e97cd92095c9c0e6713503f9
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-07-23 10:24:48 +0200

    client_lib: handshaking: Simplify handshake procedure, based on the new protocol

--- src/client_lib/handshaking.rs
+++ src/client_lib/handshaking.rs
@@ -11,16 +11,16 @@ use tokio::{
     sync::{mpsc, oneshot},
 };
 
-use crate::shared_lib::socket_handling::{
-    RecvHandler, RecvHandlerError, WriteHandler, HMAC_KEY_SIZE,
-};
+use crate::shared_lib::socket_handling::{RecvHandler, WriteHandler, HMAC_KEY_SIZE};
 use crate::{
     client_lib::globals::CLIENT_COM,
-    globals::{CONNECTION_ACCEPTED, TAKEN, TIMEOUT, TOO_LONG, TOO_MANY_TRIES, TOO_SHORT},
+    globals::CONNECTION_ACCEPTED,
     shared_lib::{OutputMsg, StdinRequest},
 };
 
-/// TODO: comment, custom error, error handling
+/// TODO: comment, custom error, error handling.
+/// TODO: we could have a client that automatically retries if something went
+/// wrong with the handshake.
 pub async fn handshake(
     write_handler: &mut WriteHandler<BufWriter<OwnedWriteHalf>>,
     read_handler: &mut RecvHandler<BufReader<OwnedReadHalf>>,
@@ -38,40 +38,28 @@ pub async fn handshake(
         ))
         .await?;
 
-    loop {
-        response.clear();
+    response.clear();
 
-        let (tx, rx) = oneshot::channel::<String>();
-        let req = StdinRequest::Plain(tx);
-        stdin_req_tx.send(req).await?;
+    let (tx, rx) = oneshot::channel::<String>();
+    let req = StdinRequest::Plain(tx);
+    stdin_req_tx.send(req).await?;
 
-        tokio::select! {
-            // reading from stdin
-            nick = rx => {
-                match nick {
-                    Ok(n) => {
-                        write_handler.write_str(&n).await?;
-                    }
-                    Err(e) => {
-                        output_tx.send(OutputMsg::new_error(&format!("{}, Retry.", e))).await?;
-                    }
+    tokio::select! {
+        // reading from stdin
+        nick = rx => {
+            match nick {
+                Ok(n) => {
+                    write_handler.write_str(&n).await?;
+                }
+                Err(e) => {
+                    output_tx.send(OutputMsg::new_error(&format!("{}, Retry.", e))).await?;
                 }
             }
-            // reading the response from the server
-            r = read_handler.recv_str(&mut response) => {
-                match handles_response(output_tx, &r, &response).await {
-                        Ok(keep_trying) => {
-                            if keep_trying {
-                                continue;
-                            } else {
-                                break;
-                            }
-                        },
-                        Err(e) => {
-                            return Err(e.into());
-                        }
-                    }
-            }
+        }
+        // reading the response from the server
+        r = read_handler.recv_str(&mut response) => {
+            r?;
+            handles_response(output_tx, &response).await?;
         }
     }
 
@@ -80,73 +68,25 @@ pub async fn handshake(
 
 /// # `handshake`'s helper `handles_response`
 ///
-/// After reading sending the nickname to the server a response will be issued, outcome is the
-/// result of this interation, response is the value that resulted from the interation.
-/// The function returns an error if the connection has been interrupted, a bool otherwise,
-/// it the bool is true we can keep trying and send another nickname, if the bool is false
-/// it means the handshake has been successfull and the user can start using the chat.
-///
-/// ## Params
-///
-/// - output_tx: channel that displays eventual outputs
-/// - outcome: result of the communication between client and server
-/// - response: values returned by the server.
+/// Handles the server's acceptance message. If Ok is returned, the client is been accepted,
+/// otherwise something went wrong.
 async fn handles_response(
     output_tx: &mut mpsc::Sender<OutputMsg>,
-    outcome: &Result<(), RecvHandlerError>,
     response: &str,
-) -> Result<bool, anyhow::Error> {
-    match outcome {
-        Ok(_) => {
-            if response == CONNECTION_ACCEPTED {
-                output_tx.send(OutputMsg::new(&format!("You have been accepted, type \"{}\" for displaying all the avaible commands", CLIENT_COM.trim()))).await?;
-                return Ok(false);
-            } else if response == TOO_MANY_TRIES {
-                let msg = "Connection refused due to: too many tries.";
-                output_tx.send(OutputMsg::new(msg)).await?;
-                return Err(anyhow::anyhow!(msg));
-            } else if response == TOO_SHORT {
-                output_tx
-                    .send(OutputMsg::new("The nickname you chose is to short, retry."))
-                    .await?;
-            } else if response == TOO_LONG {
-                output_tx
-                    .send(OutputMsg::new("The nickname you chose is to long, retry."))
-                    .await?;
-            } else if response == TIMEOUT {
-                let msg = "Timeout reached, try and reconnect.";
-                output_tx.send(OutputMsg::new(msg)).await?;
-                return Err(anyhow::anyhow!(msg));
-            } else if response == TAKEN {
-                output_tx
-                    .send(OutputMsg::new(&format!(
-                        "The nickname has already been taken, please choose another one.",
-                    )))
-                    .await?;
-            } else {
-                output_tx.send(OutputMsg::new("Retry")).await?;
-            }
-        }
-        Err(e) => {
-            match e {
-                RecvHandlerError::ConnectionInterrupted => {
-                    let msg = "Connection reset by server.";
-                    output_tx
-                        .send(OutputMsg::new_error("Connection reset by server."))
-                        .await?;
-                    return Err(anyhow::anyhow!(msg));
-                }
-                _others => {}
-            }
-
-            output_tx
-                .send(OutputMsg::new_error(&format!("{}", e)))
-                .await?;
-
-            return Err(anyhow::anyhow!(format!("{}", e)));
-        }
+) -> Result<(), anyhow::Error> {
+    if response == CONNECTION_ACCEPTED {
+        output_tx
+            .send(OutputMsg::new(&format!(
+                "You have been accepted, type \"{}\" for displaying all the avaible commands",
+                CLIENT_COM.trim()
+            )))
+            .await?;
+        return Ok(());
+    } else {
+        let msg = "Connection refused due to: {response}.";
+        output_tx.send(OutputMsg::new(msg)).await?;
+        return Err(anyhow::anyhow!(msg));
     }
-    Ok(true)
 }
 
 async fn key_exchange(
